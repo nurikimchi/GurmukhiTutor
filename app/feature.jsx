a29c63cb4313d7bd28c52e898a08bf66806b9222
Caramel4764 <nina.dong4764@gmail.com>
2024-10-22 01:08:22 -0700

    feat: implement stage 2 for 3 section and update xp gain

--- app/feature.jsx
+++ app/feature.jsx
@@ -395,8 +395,99 @@ export default function Feature() {
 				/>
 				</Pressable></>)
 			},
-		}
+		},
+		{
+			stage: 2,
+			section: 4,
+			renderLesson: function() {
+				return (<><View style={{ zIndex: 2, position: "absolute" }}>
+					<SVGItem
+						visible={true}
+						
+					/>
+				</View>
+				<Pressable
+					style={{ backgroundColor: "transparent", zIndex: 5 }}
+					onPressIn={() => pauseAllAnimation()}
+					onPressOut={() => handleGetPath()}
+				>
+				<Canvas
+					ref={canvasRef}
+					height={100}
+					width={100}
+					style={{
+						backgroundColor: "transparent",
+						position: "relative",
+						zIndex: 3,
+					}}
+					zIndex={100}
+          enabled={ableToDraw}
+				/>
+				</Pressable></>)
+			},
+		},
+		{
+			stage: 2,
+			section: 5,
+			renderLesson: function() {
+				return (<><View style={{ zIndex: 2, position: "absolute" }}>
+					<SVGItem
+						visible={true}
+						
+					/>
+				</View>
+				<Pressable
+					style={{ backgroundColor: "transparent", zIndex: 5 }}
+					onPressIn={() => pauseAllAnimation()}
+					onPressOut={() => handleGetPath()}
+				>
+				<Canvas
+					ref={canvasRef}
+					height={100}
+					width={100}
+					style={{
+						backgroundColor: "transparent",
+						position: "relative",
+						zIndex: 3,
+					}}
+					zIndex={100}
+          enabled={ableToDraw}
+				/>
+				</Pressable></>)
+			},
+		},
+		{
+			stage: 2,
+			section: 6,
+			renderLesson: function() {
+				return (<><View style={{ zIndex: 2, position: "absolute" }}>
+					<SVGItem
+						visible={true}
+						
+					/>
+				</View>
+				<Pressable
+					style={{ backgroundColor: "transparent", zIndex: 5 }}
+					onPressIn={() => pauseAllAnimation()}
+					onPressOut={() => handleGetPath()}
+				>
+				<Canvas
+					ref={canvasRef}
+					height={100}
+					width={100}
+					style={{
+						backgroundColor: "transparent",
+						position: "relative",
+						zIndex: 3,
+					}}
+					zIndex={100}
+          enabled={ableToDraw}
+				/>
+				</Pressable></>)
+			},
+		},
 	]
+	
 	const canvasRef = useRef(null);
 
 	const params = useLocalSearchParams();
@@ -410,7 +501,19 @@ export default function Feature() {
 	const { sqrt, pow } = Math;
 
 	const { itemLetter } = useLocalSearchParams();
-
+	function calcXPGained() {
+		let total = 0;
+		lessonSections.forEach((section)=>{
+			if (section.stage == 1) {
+				total+=5;
+			} else if (section.stage == 2) {
+				total+=10;
+			} else if (section.stage == 3) {
+				total+=15;
+			}
+		})
+		return total;
+	}
 	function euclideanDistance(p1, p2) {
 		return sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2));
 	}
@@ -690,10 +793,12 @@ export default function Feature() {
 					<path stroke-linecap="round" stroke-linejoin="round" d="M19.114 5.636a9 9 0 0 1 0 12.728M16.463 8.288a5.25 5.25 0 0 1 0 7.424M6.75 8.25l4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z" />
 			</svg>				
 			</View>
-
-        {completeButton && (<LessonButton text={currLesson == lessonSections.length-1?"Collect 10 EXP":"Continue"} onPress={() => {
+			{
+			}
+        {
+				completeButton && (<LessonButton text={currLesson == lessonSections.length-1?`Collect ${calcXPGained()} EXP`:"Continue"} onPress={() => {
 					if (currLesson == lessonSections.length-1) {
-						handleCollectEXP(10)
+						handleCollectEXP(calcXPGained)
 					}
 					else if (currLesson < lessonSections.length) {
 						resetCanvas();
